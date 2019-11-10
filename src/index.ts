e629fea2cf0a27dc89c5ca20607cb7a161628f71
Michael Porter <m_porter@live.co.uk>
2019-11-10 17:59:02 +0000

    Restructured index and added help command

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { RedownloadStatus } from './sonarr';
 import { redownloadTV } from './plexbot-actions';
 import SonarrError from './exceptions/SonarrError';
 
+const redownloadCommand = 'plexbot redownload';
+
+
 client.on('ready', () => {
   console.log(`logged in as ${client.user.tag}`);
 });
@@ -20,55 +23,61 @@ client.on('message', async (msg: Message) => {
     return;
   }
 
-  let redownloadCommand = 'plexbot redownload';
   if (content.startsWith(redownloadCommand)) {
-    // Lets go boi
-    let args = content.substring(redownloadCommand.length).split(',').map(i => i.trim());
-
-    if (args.length === 0) {
-      await didNotUnderstandArgs(msg);
-      return;
-    }
-
-    let mediaType = args[0];
-    if (mediaType !== 'movie' && mediaType !== 'show') {
-      await didNotUnderstandArgs(msg);
-      return;
-    }
-
-    try {
-      if (mediaType == 'movie') {
-        msg.reply('I can\'t handle movies yet!');
-      }
-
-      if (mediaType === 'show') {
-        if (args.length != 3) {
-          await didNotUnderstandArgs(msg);
-          return;
-        }
-
-        console.log('triggering download');
-        let redownloadStatus = await redownloadTV(args[1], args[2]);
-
-        if (redownloadStatus === RedownloadStatus.TRIGGERED_DOWNLOAD) {
-          msg.reply(`I triggered a download for ${args[1]} - ${args[2]}`);
-        } else if (redownloadStatus === RedownloadStatus.CURRENTLY_DOWNLOADING) {
-          msg.reply(`I'm in the middle of downloading ${args[1]} - ${args[2]}`);
-        }
-      }
-
-    } catch (e) {
-      if (e instanceof SonarrError) {
-        await msg.reply(e.message);
-      } else {
-        console.error(e);
-      }
-    }
-
-
+    await redownload(msg, content);
   }
 
+  if (content.startsWith('plexbot help')) {
+    await msg.reply('Try plexbot redownload,show,<showName>,<episodeNumber>');
+  }
 });
+
+async function redownload(msg: Message, content: string) {
+  let args = content.substring(redownloadCommand.length).split(',').map(i => i.trim());
+
+  if (args.length === 0) {
+    await didNotUnderstandArgs(msg);
+    return;
+  }
+
+  let mediaType = args[0];
+  if (mediaType !== 'movie' && mediaType !== 'show') {
+    await didNotUnderstandArgs(msg);
+    return;
+  }
+
+  try {
+    if (mediaType == 'movie') {
+      await msg.reply('I can\'t handle movies yet!');
+      return;
+    }
+
+    if (mediaType === 'show') {
+      if (args.length != 3) {
+        await didNotUnderstandArgs(msg);
+        return;
+      }
+
+      console.log('triggering download');
+      let redownloadStatus = await redownloadTV(args[1], args[2]);
+
+      if (redownloadStatus === RedownloadStatus.TRIGGERED_DOWNLOAD) {
+        msg.reply(`I triggered a download for ${args[1]} - ${args[2]}`);
+      } else if (redownloadStatus === RedownloadStatus.CURRENTLY_DOWNLOADING) {
+        msg.reply(`I'm in the middle of downloading ${args[1]} - ${args[2]}`);
+      }
+    }
+
+  } catch (e) {
+    if (e instanceof SonarrError) {
+      await msg.reply(e.message);
+    } else {
+      await msg.reply('Something went wrong :(');
+      console.error(e);
+    }
+  }
+
+}
 
 async function didNotUnderstandArgs(msg: Message) {
   await msg.reply(`I don't understand your command, try plexbot redownload,show,<showName>,<episodeNumber>`);
