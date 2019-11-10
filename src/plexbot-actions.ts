c9eb8795fd184d3b893714fda29840ae642a4638
Michael Porter <m_porter@live.co.uk>
2019-11-10 22:19:22 +0000

    Refactored sonarr away from being a class

--- src/plexbot-actions.ts
+++ src/plexbot-actions.ts
@@ -1,12 +1,10 @@
-import Sonarr from './sonarr';
 import { RedownloadStatus } from './types/types';
-
-const sonarr = new Sonarr();
+import { redownloadShow } from './sonarr';
 
 export async function redownloadTV(mediaName: string, episodeNumber: string): Promise<RedownloadStatus> {
   if (!episodeNumber) {
     throw new Error('You must provide an episode number for TV! (format example: S01E03');
   }
 
-  return await sonarr.redownloadShow(mediaName, episodeNumber);
+  return await redownloadShow(mediaName, episodeNumber);
 }
--- src/sonarr.ts
+++ src/sonarr.ts
@@ -6,109 +6,103 @@ import { Episode, QueueItem, RedownloadStatus, Show } from './types/types';
 const sonarrUrl = process.env.SONARR_URL;
 const sonarrApiKey = process.env.SONARR_API_KEY;
 
-export default class Sonarr {
-  private readonly connectionUrl: string;
+const connectionUrl = `${sonarrUrl}/api`;
 
-  constructor() {
-    this.connectionUrl = `${sonarrUrl}/api`;
+function buildConnectionString(command: string): string {
+  return `${connectionUrl}/${command}?apiKey=${sonarrApiKey}`;
+}
+
+function buildConnectionStringWithId(command: string, id: number): string {
+  return `${connectionUrl}/${command}/${id}/?apiKey=${sonarrApiKey}`;
+}
+
+export async function redownloadShow(showName: string, episodeNumber: string): Promise<RedownloadStatus> {
+  if (!episodeNumber.toUpperCase().match(new RegExp('S\\d\\dE\\d\\d'))) {
+    throw new SonarrError('Invalid episode number, should be format S01E01');
   }
 
-  private buildConnectionString(command: string): string {
-    return `${this.connectionUrl}/${command}?apiKey=${sonarrApiKey}`;
+  const season = episodeNumber.substring(1, 3);
+  const episode = episodeNumber.substring(4, 6);
+
+  const shows = await getShows();
+
+  const matchingShow = await findMatchingShow(shows, showName);
+
+  if (!matchingShow) {
+    throw new SonarrError(`Could not find show with name ${showName} ¯\\_(ツ)_/¯`);
   }
 
-  private buildConnectionStringWithId(command: string, id: number): string {
-    return `${this.connectionUrl}/${command}/${id}/?apiKey=${sonarrApiKey}`;
+  console.log('Getting episode');
+  const episodesForShow = await getEpisodesForShow(matchingShow);
+
+  const matchingEpisode = episodesForShow.find(
+    i => zeroPad(i.episodeNumber, 2) === episode && zeroPad(i.seasonNumber, 2) === season,
+  );
+
+  if (!matchingEpisode) {
+    throw new SonarrError(`Could not find episode ${episodeNumber} inside ${showName}`);
   }
 
-  public async redownloadShow(showName: string, episodeNumber: string): Promise<RedownloadStatus> {
-    if (!episodeNumber.toUpperCase().match(new RegExp('S\\d\\dE\\d\\d'))) {
-      throw new SonarrError('Invalid episode number, should be format S01E01');
-    }
-
-    const season = episodeNumber.substring(1, 3);
-    const episode = episodeNumber.substring(4, 6);
-
-    const shows = await this.getShows();
-
-    const matchingShow = await this.findMatchingShow(shows, showName);
-
-    if (!matchingShow) {
-      throw new SonarrError(`Could not find show with name ${showName} ¯\\_(ツ)_/¯`);
-    }
-
-    console.log('Getting episode');
-    const episodesForShow = await this.getEpisodesForShow(matchingShow);
-
-    const matchingEpisode = episodesForShow.find(
-      i => this.zeroPad(i.episodeNumber, 2) === episode && this.zeroPad(i.seasonNumber, 2) === season,
-    );
-
-    if (!matchingEpisode) {
-      throw new SonarrError(`Could not find episode ${episodeNumber} inside ${showName}`);
-    }
-
-    if (matchingEpisode.episodeFileId !== 0) {
-      console.log('Found file for episode, deleting');
-      await this.deleteEpisode(matchingEpisode);
-    }
-
-    const queue = await this.getActivityQueue();
-    if (queue.find(i => i.episode.id === matchingEpisode.id)) {
-      return RedownloadStatus.CURRENTLY_DOWNLOADING;
-    }
-
-    console.log('Triggering download for episode');
-    await this.searchForEpisode(matchingEpisode.id);
-
-    return RedownloadStatus.TRIGGERED_DOWNLOAD;
+  if (matchingEpisode.episodeFileId !== 0) {
+    console.log('Found file for episode, deleting');
+    await deleteEpisode(matchingEpisode);
   }
 
-  private findMatchingShow(shows: Show[], showName: string): Show | null {
-    const exactMatch = shows.find(i => i.title.toLowerCase() === showName.toLowerCase());
-    if (exactMatch) {
-      return exactMatch;
-    }
-
-    const fuzzySet = FuzzySet(shows.map(i => i.title));
-    const closestMatches = fuzzySet.get(showName);
-
-    if (closestMatches) {
-      throw new SonarrError(`Couldn't find show with name ${showName}, did you mean ${closestMatches[0][1]}?`);
-    }
-
-    return null;
+  const queue = await getActivityQueue();
+  if (queue.find(i => i.episode.id === matchingEpisode.id)) {
+    return RedownloadStatus.CURRENTLY_DOWNLOADING;
   }
 
-  private async getEpisodesForShow(show: Show): Promise<Episode[]> {
-    const url = `${this.buildConnectionString('episode')}&seriesId=${show.id}`;
-    const axiosResponse = await axios.get<Episode[]>(url);
-    return axiosResponse.data;
+  console.log('Triggering download for episode');
+  await searchForEpisode(matchingEpisode.id);
+
+  return RedownloadStatus.TRIGGERED_DOWNLOAD;
+}
+
+function findMatchingShow(shows: Show[], showName: string): Show | null {
+  const exactMatch = shows.find(i => i.title.toLowerCase() === showName.toLowerCase());
+  if (exactMatch) {
+    return exactMatch;
   }
 
-  private async getShows(): Promise<Show[]> {
-    // TODO: implement redis
-    const axiosResponse = await axios.get<Show[]>(this.buildConnectionString('series'));
-    return axiosResponse.data;
+  const fuzzySet = FuzzySet(shows.map(i => i.title));
+  const closestMatches = fuzzySet.get(showName);
+
+  if (closestMatches) {
+    throw new SonarrError(`Couldn't find show with name ${showName}, did you mean ${closestMatches[0][1]}?`);
   }
 
-  private async deleteEpisode(episode: { episodeFileId: number }): Promise<void> {
-    const url = this.buildConnectionStringWithId('episodeFile', episode.episodeFileId);
-    await axios.delete(url);
-  }
+  return null;
+}
 
-  private async searchForEpisode(episodeId: number): Promise<void> {
-    const url = this.buildConnectionString('command');
-    await axios.post(url, { name: 'EpisodeSearch', episodeIds: [episodeId] });
-  }
+async function getEpisodesForShow(show: Show): Promise<Episode[]> {
+  const url = `${buildConnectionString('episode')}&seriesId=${show.id}`;
+  const axiosResponse = await axios.get<Episode[]>(url);
+  return axiosResponse.data;
+}
 
-  private zeroPad(input: number, length: number): string {
-    return (Array(length + 1).join('0') + input).slice(-length);
-  }
+async function getShows(): Promise<Show[]> {
+  // TODO: implement redis
+  const axiosResponse = await axios.get<Show[]>(buildConnectionString('series'));
+  return axiosResponse.data;
+}
 
-  private async getActivityQueue(): Promise<QueueItem[]> {
-    const url = this.buildConnectionString('queue');
-    const axiosResponse = await axios.get<QueueItem[]>(url);
-    return axiosResponse.data;
-  }
+async function deleteEpisode(episode: { episodeFileId: number }): Promise<void> {
+  const url = buildConnectionStringWithId('episodeFile', episode.episodeFileId);
+  await axios.delete(url);
+}
+
+async function searchForEpisode(episodeId: number): Promise<void> {
+  const url = buildConnectionString('command');
+  await axios.post(url, { name: 'EpisodeSearch', episodeIds: [episodeId] });
+}
+
+function zeroPad(input: number, length: number): string {
+  return (Array(length + 1).join('0') + input).slice(-length);
+}
+
+async function getActivityQueue(): Promise<QueueItem[]> {
+  const url = buildConnectionString('queue');
+  const axiosResponse = await axios.get<QueueItem[]>(url);
+  return axiosResponse.data;
 }
